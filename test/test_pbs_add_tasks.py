ef2e81bca46f576edc069a82ca3dcccfb42b57f4
Daniel Lombraña González <teleyinex@gmail.com>
2014-07-03 12:16:06 +0200

    Tests for creating tasks.

--- test/test_pbs_add_tasks.py
+++ test/test_pbs_add_tasks.py
@@ -11,8 +11,48 @@ class TestHelpers(TestDefault):
     """Test class for pbs.helpers."""
 
     @patch('helpers.find_app_by_short_name')
-    def test_add_tasks(self, find_mock):
-        """Test add_tasks works."""
+    def test_add_tasks_json_with_info(self, find_mock):
+        """Test add_tasks json with info field works."""
+        project = MagicMock()
+        project.name = 'name'
+        project.short_name = 'short_name'
+        project.description = 'description'
+        project.info = dict()
+
+        find_mock.return_value = project
+
+        tasks = MagicMock()
+        tasks.read.return_value = json.dumps([{'info': {'key': 'value'}}])
+
+        pbclient = MagicMock()
+        pbclient.create_task.return_value = {'id': 1, 'info': {'key': 'value'}}
+        self.config.pbclient = pbclient
+        res = _add_tasks(self.config, tasks, 'json', 0, 30)
+        assert res == '1 tasks added to project: short_name', res
+
+    @patch('helpers.find_app_by_short_name')
+    def test_add_tasks_csv_with_info(self, find_mock):
+        """Test add_tasks csv with info field works."""
+        project = MagicMock()
+        project.name = 'name'
+        project.short_name = 'short_name'
+        project.description = 'description'
+        project.info = dict()
+
+        find_mock.return_value = project
+
+        tasks = MagicMock()
+        tasks.read.return_value = "info, value\n, %s, 2" % json.dumps({'key':'value'})
+
+        pbclient = MagicMock()
+        pbclient.create_task.return_value = {'id': 1, 'info': {'key': 'value'}}
+        self.config.pbclient = pbclient
+        res = _add_tasks(self.config, tasks, 'csv', 0, 30)
+        assert res == '1 tasks added to project: short_name', res
+
+    @patch('helpers.find_app_by_short_name')
+    def test_add_tasks_json_without_info(self, find_mock):
+        """Test add_tasks json without info field works."""
         project = MagicMock()
         project.name = 'name'
         project.short_name = 'short_name'
@@ -29,6 +69,28 @@ class TestHelpers(TestDefault):
         self.config.pbclient = pbclient
         res = _add_tasks(self.config, tasks, 'json', 0, 30)
         assert res == '1 tasks added to project: short_name', res
+
+    @patch('helpers.find_app_by_short_name')
+    def test_add_tasks_csv_without_info(self, find_mock):
+        """Test add_tasks csv with info field works."""
+        project = MagicMock()
+        project.name = 'name'
+        project.short_name = 'short_name'
+        project.description = 'description'
+        project.info = dict()
+
+        find_mock.return_value = project
+
+        tasks = MagicMock()
+        tasks.read.return_value = "key, value\n, 1, 2"
+
+        pbclient = MagicMock()
+        pbclient.create_task.return_value = {'id': 1, 'info': {'key': 'value'}}
+        self.config.pbclient = pbclient
+        res = _add_tasks(self.config, tasks, 'csv', 0, 30)
+        assert res == '1 tasks added to project: short_name', res
+
+
 
     #@patch('helpers.find_app_by_short_name')
     #def test_add_tasks_connection_error(self, find_mock):
