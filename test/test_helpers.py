162d27b6054b086098e49e8d7d2b786775acf5a1
Daniel Lombraña González <teleyinex@gmail.com>
2014-07-03 11:30:27 +0200

    Add test for update connection error

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -138,13 +138,32 @@ class TestHelpers(object):
                               long_description, tutorial)
         assert res == 'Project short_name updated!', res
 
-    #def test_update_project_connection_error(self):
-    #    """Test update_project connection error works."""
-    #    pbclient = MagicMock()
-    #    pbclient.create_app.side_effect = exceptions.ConnectionError
-    #    self.config.pbclient = pbclient
-    #    res = _create_project(self.config)
-    #    assert res == "Connection Error! The server http://server is not responding", res
+    @patch('helpers.find_app_by_short_name')
+    def test_update_project_connection_error(self, find_mock):
+        """Test update_project connection error works."""
+        project = MagicMock()
+        project.name = 'name'
+        project.short_name = 'short_name'
+        project.description = 'description'
+        project.info = dict()
+
+        find_mock.return_value = project
+
+        task_presenter = MagicMock()
+        task_presenter.read.return_value = "presenter"
+
+        tutorial = MagicMock()
+        tutorial.read.return_value = "tutorial"
+
+        long_description = MagicMock()
+        long_description.read.return_value = "long_description"
+
+        pbclient = MagicMock()
+        pbclient.update_app.side_effect = exceptions.ConnectionError
+        self.config.pbclient = pbclient
+        res = _update_project(self.config, task_presenter,
+                              long_description, tutorial)
+        assert res == "Connection Error! The server http://server is not responding", res
 
     #def test_update_project_another_error(self):
     #    """Test update_project another error works."""
