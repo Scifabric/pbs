21754f3f81e8577684faee1647ced547ed1e48c6
Daniel Lombraña González <teleyinex@gmail.com>
2014-07-03 12:17:40 +0200

    Add tests

--- test/test_pbs_add_tasks.py
+++ test/test_pbs_add_tasks.py
@@ -90,6 +90,25 @@ class TestHelpers(TestDefault):
         res = _add_tasks(self.config, tasks, 'csv', 0, 30)
         assert res == '1 tasks added to project: short_name', res
 
+    @patch('helpers.find_app_by_short_name')
+    def test_add_tasks_unknow_type(self, find_mock):
+        """Test add_tasks with unknown type works."""
+        project = MagicMock()
+        project.name = 'name'
+        project.short_name = 'short_name'
+        project.description = 'description'
+        project.info = dict()
+
+        find_mock.return_value = project
+
+        tasks = MagicMock()
+        tasks.read.return_value = "key, value\n, 1, 2"
+
+        pbclient = MagicMock()
+        pbclient.create_task.return_value = {'id': 1, 'info': {'key': 'value'}}
+        self.config.pbclient = pbclient
+        res = _add_tasks(self.config, tasks, 'doc', 0, 30)
+        assert res == "Unknown format for the tasks file. Use json or csv.", res
 
 
     #@patch('helpers.find_app_by_short_name')
