ec1dd53caa0a14fd1d6a934a4423a0bb22cb92c0
Daniel Lombraña González <teleyinex@gmail.com>
2014-07-03 11:28:06 +0200

    Add test

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -110,3 +110,45 @@ class TestHelpers(object):
         pbclient.create_app.return_value = self.error
         self.config.pbclient = pbclient
         assert_raises(SystemExit, _create_project, self.config)
+
+    @patch('helpers.find_app_by_short_name')
+    def test_update_project_create(self, find_mock):
+        """Test update_project works."""
+        project = MagicMock()
+        project.name = 'name'
+        project.short_name = 'short_name'
+        project.description = 'description'
+        project.info = dict()
+
+        find_mock.return_value = project
+
+        task_presenter = MagicMock()
+        task_presenter.read.return_value = "presenter"
+
+        tutorial = MagicMock()
+        tutorial.read.return_value = "tutorial"
+
+        long_description = MagicMock()
+        long_description.read.return_value = "long_description"
+
+        pbclient = MagicMock()
+        pbclient.update_app.return_value = {'short_name': 'short_name'}
+        self.config.pbclient = pbclient
+        res = _update_project(self.config, task_presenter,
+                              long_description, tutorial)
+        assert res == 'Project short_name updated!', res
+
+    #def test_update_project_connection_error(self):
+    #    """Test update_project connection error works."""
+    #    pbclient = MagicMock()
+    #    pbclient.create_app.side_effect = exceptions.ConnectionError
+    #    self.config.pbclient = pbclient
+    #    res = _create_project(self.config)
+    #    assert res == "Connection Error! The server http://server is not responding", res
+
+    #def test_update_project_another_error(self):
+    #    """Test update_project another error works."""
+    #    pbclient = MagicMock()
+    #    pbclient.create_app.return_value = self.error
+    #    self.config.pbclient = pbclient
+    #    assert_raises(SystemExit, _create_project, self.config)
