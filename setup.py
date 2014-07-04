b11a53464b2bcdf5ba26f43dbdb81e0a0fde44d7
Daniel Lombraña González <teleyinex@gmail.com>
2014-07-04 08:41:37 +0200

    Use pandoc to convert from Markdown to RST for pypi

--- setup.py
+++ setup.py
@@ -3,23 +3,31 @@
 
 from setuptools import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(
-    name="pbs",
+    name="pybossa-pbs",
     version="1.0",
     author="Daniel Lombraña González",
     author_email="info@pybossa.com",
     description="PyBossa command line client",
+    long_description=read_md('README.md'),
     license="AGPLv3",
     url="https://github.com/PyBossa/pbs",
-    classifiers = ['Development Status :: 0 - Production',
-                   'Environment :: Consle',
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Console',
                    'Intended Audience :: Developers',
-                   'License :: OSI Approved :: GNU Affer v3',
+                   'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',],
     py_modules=['pbs'],
     install_requires=['Click', 'pybossa-client', 'requests', 'nose', 'mock', 'coverage',
-                      'rednose'],
+                      'rednose', 'pypandoc'],
     entry_points='''
         [console_scripts]
         pbs=pbs:cli
