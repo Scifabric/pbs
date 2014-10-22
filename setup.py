c3a5068b13da578fc4a4c68e6d9f8c892c23fe19
Daniel Lombraña González <teleyinex@gmail.com>
2014-10-22 12:16:22 +0200

    Increase version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name="pybossa-pbs",
-    version="1.5",
+    version="1.6",
     author="Daniel Lombraña González",
     author_email="info@pybossa.com",
     description="PyBossa command line client",
