e836f3c558085aa0a1275546ac45b8146254ee6b
Daniel Lombraña González <teleyinex@gmail.com>
2015-01-08 11:28:45 +0100

    Refactor error as a property.

--- test/default.py
+++ test/default.py
@@ -1,23 +1,35 @@
+"""Test module for pbs client."""
 from mock import MagicMock
 import pbclient
+
+
 class TestDefault(object):
 
     """Test class for pbs.helpers."""
-
-    error = {"action": "GET",
-             "exception_cls": "NotFound",
-             "exception_msg": "(NotFound)",
-             "status": "failed",
-             "status_code": 404,
-             "target": "/api/app"}
 
     config = MagicMock()
     config.server = 'http://server'
     config.api_key = 'apikey'
     config.pbclient = pbclient
     config.project = {'name': 'name',
-                 'description': 'description',
-                 'short_name': 'short_name'}
+                      'description': 'description',
+                      'short_name': 'short_name'}
 
     def tearDown(self):
+        """Tear down method."""
         self.error['status'] = 'failed'
+
+    @property
+    def error(self, action='GET',
+              exception_cls='NotFound',
+              exception_msg='(NotFound)',
+              status='failed',
+              status_code=404,
+              target='/api/app'):
+        """Error property."""
+        return {'action': action,
+                'exception_cls': exception_cls,
+                'exception_msg': exception_msg,
+                'status': status,
+                'status_code': status_code,
+                'target': target}
