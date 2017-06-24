cac791f39572a3eb8b141e664165c9d9f3a7b907
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-24 18:19:19 +0100

    Check for database error

--- helpers.py
+++ helpers.py
@@ -35,7 +35,7 @@ import polib
 import openpyxl
 import itertools
 from requests import exceptions
-from pbsexceptions import ProjectNotFound, TaskNotFound
+from pbsexceptions import ProjectNotFound, TaskNotFound, DatabaseError
 import logging
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
@@ -363,6 +363,9 @@ def find_project_by_short_name(short_name, pbclient, all=None):
 def check_api_error(api_response):
     """Check if returned API response contains an error."""
     if type(api_response) == dict and (api_response.get('status') == 'failed'):
+        if 'ProgrammingError' in api_response.get('exception_cls'):
+            raise DatabaseError(message='PyBossa database error.',
+                                error=api_response)
         if 'project' in api_response.get('target'):
             raise ProjectNotFound(message='PyBossa Project not found',
                                   error=api_response)
