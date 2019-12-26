6dbdec49bed328dd4c2ede2a5afa8b424d5f276a
Daniel Lombraña González <teleyinex@gmail.com>
2019-12-26 17:11:03 +0100

    chore(version): bump version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except (ImportError, OSError):
 
 setup(
     name="pybossa-pbs",
-    version="2.4.7",
+    version="3.0.0",
     author="Scifabric LTD",
     author_email="info@scifabric.com",
     description="PYBOSSA command line client",
